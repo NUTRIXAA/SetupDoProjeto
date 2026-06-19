97cb9d3f6835456d05e2af9ab6ff7daf17a31be4
AnnaEllyca <ellycaanna0504@gmail.com>
2026-06-18 21:12:37 -0300

    Implementação inicial das telas Nutrixa

--- Entregavel2/refeicao/urls.py
+++ Entregavel2/refeicao/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login, name='login'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('planejamento/', views.planejamento, name='planejamento'),
+    path('detalhes/', views.detalhes, name='detalhes'),
+    path('cadastro-refeicao/', views.cadastro_refeicao, name='cadastro_refeicao'),
+    path('compras/', views.compras, name='compras'),
+    path('perfil/', views.perfil, name='perfil'),
 ]
--- Entregavel2/refeicao/views.py
+++ Entregavel2/refeicao/views.py
@@ -2,3 +2,24 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'refeicao/index.html')
+
+def login(request):
+    return render(request, 'refeicao/login.html')
+
+def cadastro(request):
+    return render(request, 'refeicao/cadastro.html')
+
+def planejamento(request):
+    return render(request, 'refeicao/planejamento.html')
+
+def detalhes(request):
+    return render(request, 'refeicao/detalhes.html')
+
+def cadastro_refeicao(request):
+    return render(request, 'refeicao/cadastro_refeicao.html')
+
+def compras(request):
+    return render(request, 'refeicao/compras.html')
+
+def perfil(request):
+    return render(request, 'refeicao/perfil.html')
